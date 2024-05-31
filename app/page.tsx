bffb3c0a7fd5e9427b5574f2231ef9b3f6bfbfa8
StephanieZgt <99921098+StephanieZgt@users.noreply.github.com>
2024-05-31 18:14:16 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import HeroSection from "@/components/HeroSection";
+import SocialLinks from "../src/components/SocialLinks";
+import "./globals.css";
+import SkillsSection from "@/components/SkillsSection";
+import Footer from "@/components/Footer";
+import ProjectSection from "@/components/ProjectSection";
+export default function Home() {
+  return (
+    <main>
+      <div className="px-4 md:px-8 xl:px-32 relative">
+        <div className="absolute top-0 left-0 right-0 z-50 px-4 md:px-8 xl:px-32">
+          <SocialLinks />
+        </div>
+        <HeroSection />
+        <hr className="w-full text-white" />
+        <SkillsSection />
+        <hr className="md:hidden w-full text-white" />
+        <ProjectSection />
+      </div>
+      <Footer />
+    </main>
+  );
+}
